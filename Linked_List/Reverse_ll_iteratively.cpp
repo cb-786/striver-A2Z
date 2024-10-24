5d0e5480df7bdddd32be17988961b96c455ae559
chbansal <150966009+cb-786@users.noreply.github.com>
2024-10-25 03:56:10 +0530

    Add files via upload

---
+++ Linked_List/Reverse_ll_iteratively.cpp
@@ -0,0 +1,17 @@
+ ListNode* reverseList(ListNode* head) {
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        
+        
+        while(curr!=NULL) {
+            ListNode *nextnode = curr->next;
+            
+            curr->next = prev;
+            prev=curr;
+            curr=nextnode;
+        }
+
+        return prev;
+
+    }
