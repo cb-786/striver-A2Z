eb3f40f807028b3132f18e218db830d11a539fec
chbansal <150966009+cb-786@users.noreply.github.com>
2025-02-11 14:30:05 +0530

    Add files via upload

---
+++ POTD/clear_digits.cpp
@@ -0,0 +1,13 @@
+string clearDigits(string s) {
+    string ans;
+    for(int i=0;i<s.size();i++) {
+        if(s[i]>='a' && s[i]<='z') {
+            ans.push_back(s[i]);
+        }
+
+        else {
+            ans.pop_back();
+        }
+    }
+    return ans;
+}
