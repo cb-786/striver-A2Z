2874ea7c92574b158d00855b559eb47c76dd55f5
chbansal <150966009+cb-786@users.noreply.github.com>
2025-05-10 18:37:43 +0530

    Add files via upload

---
+++ Array/move_zeros.cpp
@@ -0,0 +1,14 @@
+void moveZeroes(vector<int>& nums) {
+    int j=0;
+    while(j<nums.size() && nums[j]!=0) {
+        j++;
+    }
+
+    for(int i=j+1;i<nums.size();i++) {
+        if(nums[i]!=0) {
+            swap(nums[i],nums[j]);
+            j++;
+        }
+    }
+
+}
