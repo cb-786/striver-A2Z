6339c9b76b12f1db22d3fadcf6eecd91244dc520
chbansal <150966009+cb-786@users.noreply.github.com>
2025-06-07 11:39:49 +0530

    Add files via upload

---
+++ Array/array_leaders.cpp
@@ -0,0 +1,15 @@
+vector<int> leaders(vector<int>& arr) {
+    vector<int> ans;
+    int maxi = INT_MIN;
+    for(int i=arr.size()-1;i>=0;i--) {
+        if(arr[i]>=maxi) {
+            ans.push_back(arr[i]);
+        }
+        
+        maxi = max(maxi,arr[i]);
+    }
+    
+    reverse(ans.begin(),ans.end());
+    return ans;
+    
+}
