b7f414a2c2c4f304a5130eb853332309ef12005e
chbansal <150966009+cb-786@users.noreply.github.com>
2025-07-06 17:58:14 +0530

    Add files via upload

---
+++ heaps/merge_k_sorted_arrays.cpp
@@ -0,0 +1,19 @@
+vector<int> mergeKArrays(vector<vector<int>> arr, int K) {
+    priority_queue<int,vector<int>,greater<int>> pq;
+    vector<int> ans;
+    for(int i=0;i<arr.size();i++) {
+        for(int j=0;j<arr[i].size();j++) {
+            pq.push(arr[i][j]);
+        }
+    }
+    
+    while(!pq.empty()) {
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    
+    return ans;
+    
+    
+    
+}
