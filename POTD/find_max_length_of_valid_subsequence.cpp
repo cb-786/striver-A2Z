2515acb9a85e781735141cce051593bcf2d4d645
chbansal <150966009+cb-786@users.noreply.github.com>
2025-07-16 17:03:56 +0530

    Add files via upload

---
+++ POTD/find_max_length_of_valid_subsequence.cpp
@@ -0,0 +1,14 @@
+int maximumLength(vector<int>& nums) {
+        int res = 0;
+        vector<vector<int>> patterns = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        for (auto& pattern : patterns) {
+            int cnt = 0;
+            for (int num : nums) {
+                if (num % 2 == pattern[cnt % 2]) {
+                    cnt++;
+                }
+            }
+            res = max(res, cnt);
+        }
+        return res;
+    }
