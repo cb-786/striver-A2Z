7c961cb262f13ce96df14e21ab0bbbc3654868b3
chbansal <150966009+cb-786@users.noreply.github.com>
2025-08-17 13:49:33 +0530

    Add files via upload

---
+++ Binary_tree/is_possible_unique_binary_tree.cpp
@@ -0,0 +1,10 @@
+bool isPossible(int a, int b) {
+        // code here
+        if(a==2 && b==2) return 0;
+        
+        if(a==2 || b==2) return 1;
+        
+        
+        
+        return 0;
+    }
