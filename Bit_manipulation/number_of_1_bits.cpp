b69ebf026304f8c7e06be123413e5e8e497643cc
chbansal <150966009+cb-786@users.noreply.github.com>
2025-10-12 18:28:50 +0530

    Add files via upload

---
+++ Bit_manipulation/number_of_1_bits.cpp
@@ -0,0 +1,9 @@
+int hammingWeight(int n) {
+        int ans =0;
+        while(n>1) {
+            if(n%2==1) ans++;
+            n=n/2;
+        }
+        if(n==1) ans++;
+        return ans;
+    }
