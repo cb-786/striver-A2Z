d1315b06abcccdbb274df935502afba500d93c05
chbansal <150966009+cb-786@users.noreply.github.com>
2026-03-25 15:46:12 +0530

    Add files via upload

---
+++ strings/largest_odd_number.cpp
@@ -0,0 +1,12 @@
+ string largestOddNumber(string num) {
+        int n = num.size();
+        for(int i = n-1;i>=0;i--) {
+            if((num[i]-'0')%2==1) {
+               string ans =  num.substr(0,i+1);
+                return ans;
+            }
+        }
+
+        return "";
+
+    }
