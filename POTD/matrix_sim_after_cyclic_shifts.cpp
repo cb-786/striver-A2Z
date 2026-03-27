581c4dd8b33a0db1a3ee0f9ed8341b101b3259b0
chbansal <150966009+cb-786@users.noreply.github.com>
2026-03-27 12:20:19 +0530

    Add files via upload

---
+++ POTD/matrix_sim_after_cyclic_shifts.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+       int n = mat[0].size();
+    
+    k = k % n; 
+    
+    for (int i = 0; i < mat.size(); i++) {
+        for (int j = 0; j < n; j++) {
+           
+            if (mat[i][j] != mat[i][(j + k) % n]) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+    }
+};
