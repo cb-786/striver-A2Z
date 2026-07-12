811703d16529301a74ffbdfca344adb51e1b5ede
Chirag Bansal <chiragbansal192005@gmail.com>
2026-07-12 15:23:45 +0530

    xor_range

---
+++ Bit_manipulation/find_xor_of_a_number_rangel.cpp
@@ -0,0 +1,14 @@
+int helper(int n) {
+        if(n%4==1) return 1;
+        if(n%4==2) return n+1;
+        if(n%4==3) return 0;
+        if(n%4==0) return n;
+    }
+  
+  
+  
+    int findXOR(int l, int r) {
+        // code here
+        
+        return (helper(r)^helper(l-1));
+    }
